c1708318783063d00764f9c18071c8c83719cad7
Lukas <lukas.johansson@skola.taby.se>
2024-04-25 13:06:52 +0200

    eef

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Victor är en liten skogstomte han är en liten och kort gnom')
+app.get('/greet', (req, res) => {
+  const name = req.query.name || 'Guest'
+  let html = `<html> <head> </head><body> <h1> Hej ${name}! </h1> </body> </html>`
+  res.send(html)
 })
 
 app.listen(port, () => {
